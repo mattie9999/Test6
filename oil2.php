fa6fc6e56909dcbd72406a8e17707db12a8bb208
mattaneeya <mattie9999@gmail.com>
2014-04-02 14:15:04 +0700

    test first commit

---
+++ oil2.php
@@ -0,0 +1,68 @@
+
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=TIS-620">
+<meta http-equiv="Content-Language" content="th">
+<meta name="Description" content="Oil Price">
+</head>
+
+<body>
+
+	<!--<div align=center><table border=0 cellspacing=0 cellpadding=0 width=1000 bgcolor=#ffffff style="border-collapse: collapse">
+	<tr><td bgcolor=#ffffff valign=top align=center><br>&nbsp;<br></td></tr>
+<tr><td bgcolor=#ffffff valign=top align=center><h2>Oil Price</h2><br><br></td></tr>
+</table></div> --!>
+
+<!--<tr><td bgcolor=#e5e5e5 valign=top align=center background="images/shadow-gray.gif" height=5><img src="images/clear.gif" width=1 height=5></td></tr> --!>
+
+<!--<tr><td bgcolor=#ececec valign=top align=center>Reported by Bureau of Petroleum and Petrochemical Policy, EPPO<br>รายงานโดยสำนักนโยบายปิโตรเลียมและปิโตรเคมี สนพ.<br>
+Tel 0-2612-1555 ext 562 - 568</td></tr> !-->
+
+<!-- !-->
+<div align=center><table border=0 cellspacing=0 cellpadding=0 width=1000 bgcolor=#ffffff style="border-collapse: collapse">
+<tr><td bgcolor=#000000 valign=top width=500>
+<div align=center><table border=0 cellspacing=0 cellpadding=0 width=1000 bgcolor=#ffffff>
+
+
+<tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr><tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr><tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr>
+
+
+<tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>
+
+<p><script type="text/javascript" src="http://www.oil-price.net/TABLE2/gen.php?lang=en"></script><noscript></p>
+<p><a href="http://www.oil-price.net/dashboard.php?lang=en#TABLE2">To get the oil price, please enable Javascript.</a></noscript></p>
+<p><script type="text/javascript" src="http://www.oil-price.net/widgets/natural_gas_large/gen.php?lang=en#natural_gas_large"> </script><noscript> To get the <a href="http://www.oil-price.net">natural gas price</a>, please enable Javascript. </noscript></p>
+<p><script type="text/javascript" src="http://www.gas-cost.net/widget.php?lang=en"></script><noscript> <a href="http://gas-cost.net/dashboard.php?lang=en">To get gasoline price, please enable Javascript.</a></noscript></p>
+<p><script type="text/javascript" src="http://www.gas-cost.net/syndicate_usd.php?lang=en"></script></p>
+</script>
+</td>
+
+<td nowrap align=center width=50% valign=top bgcolor=#000000>
+<p align=center><img src="http://omrpublic.iea.org/Dashboard/prices.gif"></p>
+<p align=center><img src="http://omrpublic.iea.org/DashBoard/pricespreads.gif"><br>Source: IEA's <a href="http://omrpublic.iea.org">oilmarketreport.org</a></p>
+
+<p><img src="http://omrpublic.iea.org/DashBoard/supply.gif"></p>
+<p><img src="http://omrpublic.iea.org/DashBoard/opec.gif"></p>
+<p><img src="http://omrpublic.iea.org/DashBoard/demand.gif"><br>Source: IEA's <a href="http://omrpublic.iea.org">oilmarketreport.org</a></p>
+</td></tr>
+<tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr><tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr><tr>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td>
+<td nowrap align=center width=50% valign=top bgcolor=#000000>&nbsp;</td></tr>
+</table></div>
+</td></tr></table></div>
+
+</body>
+
+</html>
